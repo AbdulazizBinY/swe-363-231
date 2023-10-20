adcd26f262a8df25646bec26fa1ec24faec3f631
Abdulaziz <Abdulazizbinyabis@gmail.com>
2023-10-20 17:47:48 +0300

    JS_3_exercise_2

--- js/scripts.js
+++ js/scripts.js
@@ -133,3 +133,36 @@ function populateHobbies() {
 // Call the functions to populate data
 populateProjects();
 populateHobbies();
+
+
+// JavaScript for signature canvas
+let canvas = document.getElementById('signatureCanvas');
+let ctx = canvas.getContext('2d');
+let isDrawing = false;
+
+canvas.addEventListener('mousedown', () => {
+    isDrawing = true;
+    ctx.beginPath();
+});
+
+canvas.addEventListener('mousemove', draw);
+canvas.addEventListener('mouseup', () => {
+    isDrawing = false;
+    ctx.closePath();
+});
+
+function draw(event) {
+    if (!isDrawing) return;
+    ctx.lineWidth = 3;
+    ctx.lineCap = 'round';
+    ctx.strokeStyle = 'black';
+
+    ctx.lineTo(event.clientX - canvas.offsetLeft, event.clientY - canvas.offsetTop);
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.moveTo(event.clientX - canvas.offsetLeft, event.clientY - canvas.offsetTop);
+}
+
+function clearCanvas() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
