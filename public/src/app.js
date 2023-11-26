419338fbf695cdfb86865916ce6a548973622586
Abdulaziz <Abdulazizbinyabis@gmail.com>
2023-11-26 08:43:52 +0300

    BE_3_Exercises_3 and 4
    Refactored the code and added the middleware function to the contact form on the main page.

---
+++ public/src/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+
+const processForm = (req, res, next) => { // Until now, no need to store anything.
+    console.log('Processing form data:', req.body);
+    next();
+};
+
+app.post('/submit-form', processForm, (req, res) => { // This the form of the contact in the mainHTML file (main page)
+    res.send('Form submission processed');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
